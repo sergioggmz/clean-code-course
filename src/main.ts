4676b92ad81f7f50c5199b9c6595524980b86702
Sergio <sergiodejaen1234@gmail.com>
2026-03-08 15:55:26 +0100

    feat: add implementation of fruit management functions and working steps

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import './clean-code/04-homework.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
