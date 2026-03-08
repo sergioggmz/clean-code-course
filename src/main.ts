ce51fdb59f0990eaef7949d0da479ed68ebf8ddb
Sergio <sergiodejaen1234@gmail.com>
2026-03-08 19:08:25 +0100

    feat: 06-classes-a

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './clean-code/05-dry.ts'
+import './clean-code/06-classes-a.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
