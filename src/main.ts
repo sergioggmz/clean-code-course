e6ee31644565ca4e3fc5e818e703663fd66e1b41
Sergio <sergiodejaen1234@gmail.com>
2026-03-08 19:10:07 +0100

    Revert "feat: 06-classes-a"
    This reverts commit ce51fdb59f0990eaef7949d0da479ed68ebf8ddb.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './clean-code/06-classes-a.ts'
+import './clean-code/05-dry.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
